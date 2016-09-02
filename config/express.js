4a0e6a101256e6f3c59e3982d04f1f48c83d97c2
Ygor Vieira <ygorus@gmail.com>
2016-09-02 12:07:49 -0300

    Configurando express para retornar lista de fotos no formato json

--- config/express.js
+++ config/express.js
@@ -3,4 +3,14 @@ var app = express();
 
 app.use(express.static('./public'));
 
+app.get('/v1/fotos', function(req, res) {
+
+  var fotos = [
+    {_id: 1, titulo: 'Leão', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'},
+    {_id: 2, titulo: 'Leão 2', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'}
+  ];
+
+  res.json(fotos);
+});
+
 module.exports = app;

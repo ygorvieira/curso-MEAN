f59b8ce95bf02799acedd0b8affd56663d04f0ed
Ygor Vieira <ygorus@gmail.com>
2016-09-05 12:15:51 -0300

    Implementando rotas para deletar fotos e melhorias na chamada à api

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,17 +1,37 @@
 var api = {};
 
+var fotos = [
+{ _id: 1,
+  titulo: 'Leão',
+  url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+},
+{ _id: 2,
+  titulo: 'Leão 2',
+  url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+}
+];
+
 api.lista = function(req, res) {
-    var fotos = [
-    { _id: 1,
-      titulo: 'Leão',
-      url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    },
-    { _id: 2,
-      titulo: 'Leão 2',
-      url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    }
-  ];
+
   res.json(fotos);
+};
+
+api.buscaPorId = function(req, res) {
+
+  var foto = fotos.find(function(foto) {
+      return foto._id == req.params.id;
+  });
+
+  res.json(foto);
+};
+
+api.removePorId = function(req, res) {
+
+  fotos = fotos.filter(function(foto) {
+      return foto._id != req.params.id;
+  });
+
+  res.sendStatus(204);
 };
 
 module.exports = api;
--- app/routes/foto.js
+++ app/routes/foto.js
@@ -3,4 +3,8 @@ module.exports = function(app) {
   var api = app.api.foto;
 
   app.get('/v1/fotos', api.lista);
+
+  app.route('/v1/fotos/:id')
+    .get(api.buscaPorId)
+    .delete(api.removePorId);
 };

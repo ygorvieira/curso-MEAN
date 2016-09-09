314225f649774fa23adc3063ba0febc87bcfdaf0
Ygor Vieira <ygorus@gmail.com>
2016-09-08 22:00:32 -0300

    Finalizando integração com banco de dados e iniciando autenticação por token

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,62 +1,68 @@
+var mongoose = require('mongoose');
+
 var api = {};
 
-var contador = 2;
-
-var fotos = [
-{ _id: 1,
-  titulo: 'Leão',
-  url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-},
-{ _id: 2,
-  titulo: 'Leão 2',
-  url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-}
-];
+var model = mongoose.model('Foto');
 
 api.lista = function(req, res) {
 
-  res.json(fotos);
+  model
+    .find({})
+    .then(function(fotos) {
+        res.json(fotos);
+    }, function(error) {
+          console.log(error);
+          res.status(500).json(error);
+    });
 };
 
 api.buscaPorId = function(req, res) {
 
-  var foto = fotos.find(function(foto) {
-      return foto._id == req.params.id;
-  });
+  model
+    .findById(req.params.id)
+    .then(function(foto) {
+      if(!foto) throw Error('Foto não encontrada');
+      res.json(foto);
 
-  res.json(foto);
+    }, function(error) {
+          console.log(error);
+          res.status(404).json(error);
+    });
 };
 
 api.removePorId = function(req, res) {
 
-  fotos = fotos.filter(function(foto) {
-      return foto._id != req.params.id;
-  });
+  model
+    .remove({_id: req.params.id})
+    .then(function() {
+        res.sendStatus(204);
 
-  res.sendStatus(204);
+    }, function(error) {
+          console.log(error);
+          res.status(500).json(error);
+    });
 };
 
 api.adiciona = function(req, res) {
 
-  var foto = req.body;
-  foto._id = ++contador;
-  fotos.push(foto);
-
-  res.json(foto);
-};
+        model.create(req.body)
+        .then(function(foto) {
+            res.json(foto);
+        }, function(error) {
+            console.log(error);
+            res.sendStatus(500);
+        });
+    };
 
 api.atualiza = function (req, res) {
-
-  var foto = req.body;
-  var fotoId = req.params.id;
-
-  var indice = fotos.findIndex(function() {
-    return(foto._id == fotoId);
-  });
-
-  fotos[indice] = foto;
-
-  res.sendStatus(200);
+  model
+    .findByIdAndUpdate(req.params.id, req.body)
+    .then(function() {
+        res.json(foto);
+    }, function(error) {
+        console.log(error);
+        res.sendStatus(500);
+    });
 };
 
 module.exports = api;
